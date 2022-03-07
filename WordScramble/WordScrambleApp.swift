486a6a407c63614c449d4c6280a14d71dfad6f83
Hari Nur Permadi <hari.permadi@prudential.co.id>
2022-03-07 13:04:16 +0700

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Hari Permadi on 07/03/22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
