153ca447738085b00ac5c3e2effdcd72120854cd
Hari Nur Permadi <hari.permadi@prudential.co.id>
2022-03-07 17:45:52 +0700

    add function for adding new word and some validation

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -11,6 +11,10 @@ struct ContentView: View {
     @State private var usedWords = [String]()
     @State private var rootWord = ""
     @State private var newWord = ""
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    @State private var score = 0
     
     var body: some View {
         NavigationView{
@@ -18,7 +22,11 @@ struct ContentView: View {
                 Section{
                     TextField("Enter your word", text: $newWord)
                         .autocapitalization(.none)
+                    
                 }
+                
+                Text("Score: \(score)")
+                    .font(.headline)
                 
                 Section {
                     ForEach(usedWords, id: \.self) {word in
@@ -33,6 +41,23 @@ struct ContentView: View {
             .onSubmit {
                 addNewWord()
             }
+            .onAppear {
+                startGame()
+            }
+            .alert(errorTitle, isPresented: $showingError) {
+                Button("Ok", role: .cancel) {}
+            } message: {
+                Text(errorMessage)
+            }
+            .toolbar {
+                Button("Restart"){
+                    withAnimation {
+                        usedWords = [String]()
+                        startGame()
+                    }
+                }
+            }
+
         }
     }
     
@@ -40,10 +65,95 @@ struct ContentView: View {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         guard answer.count > 0 else {return}
         
+        // validation
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "used other possible")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from \(rootWord)")
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "Try again!")
+            return
+        }
+        
+        guard isLessThanThree(word: answer) else {
+            wordError(title: "Too short", message: "Used longer word!")
+            return
+        }
+        guard isStartWord(word: answer) else {
+            wordError(title: "Uh no", message: "Starting word not counted!")
+            return
+        }
+        
         withAnimation {
             usedWords.insert(answer, at: 0)
+            score += answer.count
         }
         newWord = ""
+    }
+    
+    func startGame(){
+        if let fileUrl = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let fileContents = try? String(contentsOf: fileUrl) {
+                let allWords = fileContents.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "awesome"
+                return
+            }
+        }
+        fatalError("Error when load start.txt file")
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool{
+        var temp = rootWord
+        
+        for letter in word {
+            if let pos = temp.firstIndex(of: letter) {
+                temp.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+    
+    func isLessThanThree(word: String) -> Bool {
+        if word.count < 3 {
+            return false
+        }
+        
+        return true
+    }
+    
+    func isStartWord(word: String) -> Bool {
+        if word == rootWord {
+            return false
+        }
+        
+        return true
     }
 }
 
